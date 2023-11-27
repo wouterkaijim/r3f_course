6d1fd59565c6c4230f7d86906ac048c575b80b51
Wouter360Fabriek <wouter@360fabriek.nl>
2023-11-27 10:46:07 +0100

    ✨ Add React Three Fiber Hooks

---
+++ r3f_hooks/src/App.jsx
@@ -0,0 +1,62 @@
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { button, useControls } from "leva";
+import { useRef } from "react";
+
+const Cube = (props) => {
+  const camera = useThree((state) => state.camera);
+
+  const updateFov = (fov) => {
+    camera.fov = fov;
+    camera.updateProjectionMatrix();
+  }
+
+  useControls("FOV", {
+    smallFov: button(() => updateFov(20)),
+    normalFov: button(() => updateFov(42)),
+    bigFov: button(() => updateFov(60)),
+    hugeFov: button(() => updateFov(110)),
+  })
+
+  const cube = useRef();
+
+  const { speed } = useControls("SPEED", {
+    speed: {
+      value: 0,
+      min: -12,
+      max: 12
+    }
+  })
+
+  useFrame((state, delta) => {
+    cube.current.rotation.y += speed * delta;
+  })
+
+  return (
+    <mesh {...props} ref={cube}>
+      <boxGeometry />
+      <meshStandardMaterial color={"white"} />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [0, 2, 6], fov: 42 }}>
+        <OrbitControls />
+        <Cube rotation-y={Math.PI / 4} />
+        <ContactShadows
+          position-y={-2}
+          opacity={0.5}
+          blur={2}
+          color={"pink"}
+          scale={10}
+        />
+        <Environment preset="city" />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
